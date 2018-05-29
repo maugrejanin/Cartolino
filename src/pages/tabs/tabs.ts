4d030a7a2ff22feef21402ba5785fd465457d306
Mauricio Grejanin <mgrejanin@eurekaria.com>
2018-05-29 19:11:34 -0300

    terminar em casa

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { IonicPage, NavController } from 'ionic-angular';
 
 import { Tab1Root, Tab2Root, Tab3Root, Tab4Root } from '../';
+import { IUserDataControll } from '../../models/userDataControll';
 
 @IonicPage()
 @Component({
@@ -20,12 +21,20 @@ export class TabsPage {
   tab3Title = " ";
   tab4Title = " ";
 
-  constructor(public navCtrl: NavController, public translateService: TranslateService) {
+  userLogged = false;
+
+  constructor(public navCtrl: NavController, public translateService: TranslateService, @Inject('IUserDataControll') private userDataControll: IUserDataControll) {
     translateService.get(['TAB1_TITLE', 'TAB2_TITLE', 'TAB3_TITLE', 'TAB4_TITLE']).subscribe(values => {
       this.tab1Title = values['TAB1_TITLE'];
       this.tab2Title = values['TAB2_TITLE'];
       this.tab3Title = values['TAB3_TITLE'];
       this.tab4Title = values['TAB4_TITLE'];
     });
+  }
+
+  ionViewWillEnter(){
+    this.userLogged = this.userDataControll.isUserLogged();
+    console.log(this.userDataControll.isUserLogged());
+    
   }
 }
